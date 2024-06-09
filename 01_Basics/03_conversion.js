8a9c657fff4e336a363b7c6c9898928bb894bd47
chaitanyaB12 <chaitanyabhopnikar@gmail.com>
2024-06-09 14:33:31 +0000

    Learn Conversions In JS

---
+++ 01_Basics/03_conversion.js
@@ -0,0 +1,64 @@
+let score ="chaitanya"
+
+let ValueInNumber = Number(score)
+
+//console.log(ValueInNumber);
+//it will print NaN Which is Not A number
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn)
+
+//for   1--> True 
+//      0--> False 
+// "Chai"--> True
+//    "" --> False when it is empty in string it gives us false
+
+let somescore = 44
+
+let stringNumber = Number(somescore)
+
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
